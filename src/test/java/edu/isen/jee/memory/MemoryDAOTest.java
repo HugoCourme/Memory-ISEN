e78f036956e9eae5d26b7d9edafe42673c3fc91f
LouisBoss <louis.bossut@gmail.com>
2016-02-11 11:54:59 +0100

    Fix of on test in MemoryDAOTest

--- src/test/java/edu/isen/jee/memory/MemoryDAOTest.java
+++ src/test/java/edu/isen/jee/memory/MemoryDAOTest.java
@@ -12,73 +12,91 @@ import org.junit.runner.RunWith;
 @RunWith(GuiceRunner.class)
 @Modules({ H2DBModule.class, JPAModule.class })
 public class MemoryDAOTest {
-	
-	 @Inject
-	 EntityManager em;
 
-	 @Inject
-	 MemoryDAO dao;
+	@Inject
+	EntityManager em;
 
-	 @Test
-	 public void daoIsInjected() throws Exception {
-	     assertThat(dao).isNotNull();
-	 }
-	    
-	 @Test
-	 public void itCanCreateAGame() throws Exception {
-		 MemoryAdapter game = dao.createNewGame();
-	     assertThat(game).isNotNull();
+	@Inject
+	MemoryDAO dao;
 
-	     String token = game.getToken();
-	     assertThat(game.getToken()).isNotNull();
-	     em.clear();
-	     game = dao.loadFromToken(token);
-	     assertThat(game).isNotNull();
+	@Test
+	public void daoIsInjected() throws Exception {
+		assertThat(dao).isNotNull();
+	}
 
-	 }
-	 
-	 @Test
-	 public void itCanPlayWithJPAGame() throws Exception {
-		 MemoryAdapter game = dao.createNewGame();
-		 game.returnCard(0);
-		 if (game.getCard(0).frontColorIndex != game.getCard(1).frontColorIndex) {
-			 game.returnCard(1);
-			 assertThat(game.getCard(0).side).isEqualTo(Card.Side.RECTO);
-			 assertThat(game.getCard(1).side).isEqualTo(Card.Side.RECTO);
-		}else {
+	@Test
+	public void itCanCreateAGame() throws Exception {
+		MemoryAdapter game = dao.createNewGame();
+		assertThat(game).isNotNull();
+
+		String token = game.getToken();
+		assertThat(game.getToken()).isNotNull();
+		em.clear();
+		game = dao.loadFromToken(token);
+		assertThat(game).isNotNull();
+
+	}
+
+	@Test
+	public void itCanPlayWithJPAGame() throws Exception {
+		MemoryAdapter game = dao.createNewGame();
+		game.returnCard(0);
+		if (game.getCard(0).frontColorIndex != game.getCard(1).frontColorIndex) {
+			game.returnCard(1);
+			assertThat(game.getCard(0).side).isEqualTo(Card.Side.RECTO);
+			assertThat(game.getCard(1).side).isEqualTo(Card.Side.RECTO);
+		} else {
 			game.returnCard(2);
-			 assertThat(game.getCard(0).side).isEqualTo(Card.Side.RECTO);
-			 assertThat(game.getCard(2).side).isEqualTo(Card.Side.RECTO);
+			assertThat(game.getCard(0).side).isEqualTo(Card.Side.RECTO);
+			assertThat(game.getCard(2).side).isEqualTo(Card.Side.RECTO);
 		}
-		 
-	 }
-	 
-	 @Test
-	    public void adapterManagesTurns() throws Exception {
-	        MemoryAdapter game = dao.createNewGame();
-	        assertThat(game.getCurrentPlayer()).isNotNull();
-	        assertThat(game.getCurrentPlayer()).isEqualTo(0);
-	        game.returnCard(0);
-			 if (game.getCard(0).frontColorIndex != game.getCard(1).frontColorIndex) {
-				 game.returnCard(1);
-			}else {
-				game.returnCard(2);
-			}
-			game.returnCard(3);
-	        game = dao.loadFromToken(game.getToken());
-	        assertThat(game.getCurrentPlayer()).isEqualTo(1);
-	        game.returnCard(0);
-			 if (game.getCard(0).frontColorIndex != game.getCard(1).frontColorIndex) {
-				 game.returnCard(1);
-			}else {
-				game.returnCard(2);
-			}
-			 game.returnCard(3);
-		     game = dao.loadFromToken(game.getToken());
-		     assertThat(game.getCurrentPlayer()).isEqualTo(0);
 
-	    }
-	 
-	 
+	}
+
+	@Test
+	public void adapterManagesTurns() throws Exception {
+		MemoryAdapter game = dao.createNewGame();
+		assertThat(game.getCurrentPlayer()).isNotNull();
+		assertThat(game.getCurrentPlayer()).isEqualTo(0);
+		game.returnCard(0);
+		if (game.getCard(0).frontColorIndex != game.getCard(1).frontColorIndex) {
+			game.returnCard(1);
+		} else {
+			game.returnCard(2);
+		}
+		game.returnLastCards();
+		game = dao.loadFromToken(game.getToken());
+		assertThat(game.getCurrentPlayer()).isEqualTo(1);
+		game.returnCard(0);
+		if (game.getCard(0).frontColorIndex != game.getCard(1).frontColorIndex) {
+			game.returnCard(1);
+		} else {
+			game.returnCard(2);
+		}
+		game.returnLastCards();
+		game = dao.loadFromToken(game.getToken());
+		assertThat(game.getCurrentPlayer()).isEqualTo(0);
+	}
+	
+	@Test
+	public void CanReturnCardsIfDifferent() throws Exception{
+		MemoryAdapter game = dao.createNewGame();
+		assertThat(game.getCurrentPlayer()).isNotNull();
+		assertThat(game.getCurrentPlayer()).isEqualTo(0);
+		game.returnCard(0);
+		if (game.getCard(0).frontColorIndex != game.getCard(1).frontColorIndex) {
+			game.returnCard(1);
+		} else {
+			game.returnCard(2);
+		}
+		if (!game.canReplay()) {
+			game.returnLastCards();
+		}
+		game = dao.loadFromToken(game.getToken());
+		assertThat(game.getCard(0).side).isEqualTo(Card.Side.VERSO);
+		assertThat(game.getCard(1).side).isEqualTo(Card.Side.VERSO);
+		assertThat(game.getCard(2).side).isEqualTo(Card.Side.VERSO);
+
+	}
 
 }
